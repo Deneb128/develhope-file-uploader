df7cdcf91eec16f481f30641a7e6e78ec2248c77
Roberto Inconis <roberto.inconis@outlook.com>
2024-03-01 15:56:24 +0100

    initial upload

---
+++ src/test/java/co/develhope/deneb128/fileuploader/FileUploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.deneb128.fileuploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
